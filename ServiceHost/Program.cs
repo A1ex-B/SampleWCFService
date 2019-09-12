381fcbbf0edffa89b076d755c14e96dd20966a1a
A1ex <blinov.alex.s@gmail.com>
2019-09-12 18:59:15 +0700

    Старт сервиса (пустой)

--- ServiceHost/Program.cs
+++ ServiceHost/Program.cs
@@ -14,21 +14,24 @@ namespace WCFServiceHost
             //Create a URI to serve as the base address
             Uri httpUrl = new Uri("http://localhost:8090/ReceiptService/Service");
             //Create ServiceHost
-            ServiceHost host
-            = new ServiceHost(typeof(WCFService.Service), httpUrl);
-            //Add a service endpoint
-            host.AddServiceEndpoint(typeof(WCFService.IService)
-            , new WSHttpBinding(), "");
-            //Enable metadata exchange
-            ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
-            smb.HttpGetEnabled = true;
-            host.Description.Behaviors.Add(smb);
-            //Start the Service
-            host.Open();
+            using (var host = new ServiceHost(typeof(WCFService.Service), httpUrl))
+            {
+                //Add a service endpoint
+                host.AddServiceEndpoint(typeof(WCFService.IService)
+                , new WSHttpBinding(), "");
+                //Enable metadata exchange
+                var smb = new ServiceMetadataBehavior
+                {
+                    HttpGetEnabled = true
+                };
+                host.Description.Behaviors.Add(smb);
+                //Start the Service
+                host.Open();
 
-            Console.WriteLine("Service is host at " + DateTime.Now.ToString());
-            Console.WriteLine("Host is running... Press <Enter> key to stop");
-            Console.ReadLine();
+                Console.WriteLine("Service is host at " + DateTime.Now.ToString());
+                Console.WriteLine("Host is running... Press <Enter> key to stop");
+                Console.ReadLine();
+            }
         }
     }
 }
